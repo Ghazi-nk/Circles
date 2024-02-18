078656c4fe65ef30f0d8b88b9d1ad9121d793863
Morro014 <m.alali14@icloud.com>
2024-02-18 16:18:39 +0100

    Initial Commit

---
+++ Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by Mohammed alali on 18.02.24.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
